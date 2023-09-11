11459407c94aeeb82b91ce03cc05465a80f15466
andreainno22 <andrea.innocenti12@stud.unifi.it>
2023-09-11 11:20:06 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/shopping_list_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shopping_list_project.dir/main.cpp.o"
+  "shopping_list_project"
+  "shopping_list_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shopping_list_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
